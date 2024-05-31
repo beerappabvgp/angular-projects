a26a3e7b2ebf29540479203920ce1dbce4813a57
beerappabvgp <beerappabharathb@gmail.com>
2024-05-31 14:05:19 +0530

    learn about router link in angular

--- angular-defer/src/app/app.component.ts
+++ angular-defer/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ImageComponent } from './image/image.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , WelcomeComponent , ImageComponent],
+  imports: [RouterOutlet , WelcomeComponent , ImageComponent , RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
